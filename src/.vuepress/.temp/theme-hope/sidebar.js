be9a6ab144a1e5de338cf22a9c5d24cde72b62e4
胡标 <biao.hu@baishan.com>
2022-12-01 22:39:26 +0800

    init aide-cloud

---
+++ src/.vuepress/.temp/theme-hope/sidebar.js
@@ -0,0 +1,1 @@
+export const sidebarData = {"/demo/":["page.md","markdown.md","disable.md","encrypt.md"],"/posts/":["strawberry.md","tomato.md","dragonfruit.md","cherry.md",{"text":"Apple","prefix":"apple/","collapsible":true,"children":["1.md","2.md","3.md","4.md"]},{"text":"Banana","prefix":"banana/","collapsible":true,"children":["1.md","2.md","3.md","4.md"]}]};
