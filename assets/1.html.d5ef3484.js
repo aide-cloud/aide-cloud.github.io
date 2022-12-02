8ba861c8df848ed9629edf2cdba3e85ec94c9827
aide-cloud <52439018+aide-cloud@users.noreply.github.com>
2022-12-02 09:37:06 +0000

    Deploying to gh-pages from @ aide-cloud/aide-cloud.github.io@1cef80032bae8abc4068cd3620114106cd409082 🚀

---
+++ assets/1.html.d5ef3484.js
@@ -0,0 +1,20 @@
+import{_ as n}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as a,c as s,f as e}from"./app.c9867077.js";const t={},i=e(`<h1 id="_1-初始化gin工程" tabindex="-1"><a class="header-anchor" href="#_1-初始化gin工程" aria-hidden="true">#</a> 1. 初始化gin工程</h1><h2 id="获取gin包" tabindex="-1"><a class="header-anchor" href="#获取gin包" aria-hidden="true">#</a> 获取gin包</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>go get <span class="token parameter variable">-u</span> github.com/gin-gonic/gin
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="工程文件-main-go" tabindex="-1"><a class="header-anchor" href="#工程文件-main-go" aria-hidden="true">#</a> 工程文件(main.go)</h2><div class="language-go line-numbers-mode" data-ext="go"><pre class="language-go"><code><span class="token keyword">package</span> main
+ 
+<span class="token keyword">import</span> <span class="token string">&quot;github.com/gin-gonic/gin&quot;</span>
+ 
+<span class="token keyword">func</span> <span class="token function">main</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+	<span class="token comment">// 1.创建路由</span>
+	r <span class="token operator">:=</span> gin<span class="token punctuation">.</span><span class="token function">Default</span><span class="token punctuation">(</span><span class="token punctuation">)</span>
+	<span class="token comment">// 2.绑定路由规则，执行的函数</span>
+	<span class="token comment">// gin.Context，封装了request和response</span>
+	r<span class="token punctuation">.</span><span class="token function">GET</span><span class="token punctuation">(</span><span class="token string">&quot;/&quot;</span><span class="token punctuation">,</span> <span class="token keyword">func</span><span class="token punctuation">(</span>c <span class="token operator">*</span>gin<span class="token punctuation">.</span>Context<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+		c<span class="token punctuation">.</span><span class="token function">String</span><span class="token punctuation">(</span><span class="token number">200</span><span class="token punctuation">,</span> <span class="token string">&quot;hello World!&quot;</span><span class="token punctuation">)</span>
+	<span class="token punctuation">}</span><span class="token punctuation">)</span>
+	<span class="token comment">// 3.监听端口，默认在8080</span>
+	<span class="token comment">// Run(&quot;里面不指定端口号默认为8080&quot;)</span>
+	r<span class="token punctuation">.</span><span class="token function">Run</span><span class="token punctuation">(</span><span class="token string">&quot;:8000&quot;</span><span class="token punctuation">)</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="本地运行" tabindex="-1"><a class="header-anchor" href="#本地运行" aria-hidden="true">#</a> 本地运行</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>go run main.go
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="浏览器访问" tabindex="-1"><a class="header-anchor" href="#浏览器访问" aria-hidden="true">#</a> 浏览器访问</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>http://localhost:8000
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,9),c=[i];function o(p,l){return a(),s("div",null,c)}const r=n(t,[["render",o],["__file","1.html.vue"]]);export{r as default};
