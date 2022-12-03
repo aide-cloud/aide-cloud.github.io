6059845c809794859cc69df8130f6bd99b493fe7
胡标 <biao.hu@baishan.com>
2022-12-03 23:00:17 +0800

    init aide-cloud

--- test.go
+++ test.go
@@ -2,59 +2,30 @@ package main
 
 import (
 	"fmt"
-	"time"
-
-	"github.com/fsnotify/fsnotify"
-	"github.com/spf13/viper"
+	"golang.org/x/sync/errgroup"
+	"net/http"
 )
 
-type Config struct {
-	Server   ServerConfig
-	Database DatabaseConfig
-	Redis    RedisConfig
-}
-
-type ServerConfig struct {
-	Port string
-	Host string
-}
-
-type DatabaseConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-}
-
-type RedisConfig struct {
-	Host string
-	Port string
-}
-
 func main() {
-	viper.SetConfigName("config") // 设置配置文件名
-	viper.AddConfigPath(".")      // 设置配置文件路径
-	viper.SetConfigType("json")   // 设置文件类型
-	err := viper.ReadInConfig()   // 读取配置文件
-	if err != nil {
-		panic(err)
+	var g errgroup.Group
+	urls := []string{
+		"https://www.baidu.com",
+		"https://www.google.com",
+		"https://www.github.com",
 	}
-	var config Config
-	err = viper.Unmarshal(&config)
-	if err != nil {
-		panic(err)
+	for _, url := range urls {
+		url := url
+		g.Go(func() error {
+			resp, err := http.Get(url)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+			fmt.Println(url, resp.Status)
+			return nil
+		})
 	}
-	fmt.Println(config)
-	viper.WatchConfig()
-	viper.OnConfigChange(func(e fsnotify.Event) {
-		fmt.Println("配置文件修改了")
-		err = viper.Unmarshal(&config)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(config)
-	})
-	for {
-		time.Sleep(time.Second)
+	if err := g.Wait(); err != nil {
+		fmt.Println(err)
 	}
 }
