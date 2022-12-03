d229d32ba1505521ee0e2ec3e40483a00914dce5
胡标 <biao.hu@baishan.com>
2022-12-03 14:32:07 +0800

    init aide-cloud

--- test.go
+++ test.go
@@ -3,16 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	for {
-		select {
-		case <-ch1:
-			fmt.Println("A")
-		case ch2 <- 1:
-			fmt.Println("B")
-		default:
-			fmt.Println("C")
-		}
-	}
+
+	a := 60 &^ 13
+	fmt.Println(a)
 }
