fa9c9b212f7f02b40f4fae9c0a417081a6411779
胡标 <biao.hu@baishan.com>
2022-12-03 17:48:45 +0800

    init aide-cloud

--- test.go
+++ test.go
@@ -1,57 +1,19 @@
 package main
 
-import "fmt"
-
-type Person struct {
-	name string
-	age  int
-}
-
-type Human interface {
-	// 唱
-	sing()
-	// 跳
-	jump()
-	// 打篮球
-	playBasketball()
-	// rap
-	rap()
-	// 自我介绍
-	introduce()
-}
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	p := Person{
-		name: "张三",
-		age:  18,
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			fmt.Println("hello")
+			wg.Done()
+		}()
 	}
-	p.introduce()
-	p.sing()
-	p.jump()
-	p.playBasketball()
-	p.rap()
-}
-
-func (p Person) sayHello() {
-	fmt.Println("Hello, I'm", p.name)
-}
-
-func (p Person) sing() {
-	fmt.Println("I can sing")
-}
-
-func (p Person) jump() {
-	fmt.Println("I can jump")
-}
-
-func (p Person) playBasketball() {
-	fmt.Println("I can play basketball")
-}
-
-func (p Person) rap() {
-	fmt.Println("I can rap")
-}
-
-func (p Person) introduce() {
-	fmt.Println("I'm", p.name, "I'm", p.age, "years old")
+	wg.Wait()
+	fmt.Println("world")
 }
