0dddea6730b93b2846d9827bb479b27fc2e12bfe
胡标 <biao.hu@baishan.com>
2022-12-03 15:04:33 +0800

    init aide-cloud

--- test.go
+++ test.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-
-	a := 60 &^ 13
-	fmt.Println(a)
+	var ch chan int
+	ch = make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	time.Sleep(time.Second)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	close(ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	ch <- 1
+	fmt.Println(<-ch)
 }
