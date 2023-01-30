8f2c3bcc5fa0fb97bc72167f9a0c378f93c4ce17
梧桐 <aidecloud@163.com>
2023-01-30 11:02:37 +0800

    feat: defer介绍和用法及使用场景

--- main.go
+++ main.go
@@ -2,25 +2,10 @@ package main
 
 import "fmt"
 
-func reverseString(s *string) {
-	str := []rune(*s)
-	n := len(str)
-	for i := 0; i < n/2; i++ {
-		str[i], str[n-i-1] = str[n-i-1], str[i]
-	}
-	*s = string(str)
-}
-
 func main() {
-	str := "123456"
-	fmt.Println(str)
-	reverseString(&str)
-	fmt.Println(str)
-	s1 := str[:3]
-	s2 := str[3:]
-	reverseString(&s1)
-	reverseString(&s2)
-	str = s1 + s2
-	reverseString(&str)
-	fmt.Println(str)
+	for i := 0; i < 5; i++ {
+		defer func(i int) {
+			fmt.Println(i)
+		}(i)
+	}
 }
